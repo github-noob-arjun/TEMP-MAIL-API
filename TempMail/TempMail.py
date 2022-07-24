9211efcae65e1eebdb3afa5cc7936cd331dedb55
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-24 17:32:10 +0530

    Update TempMail.py

--- TempMail/TempMail.py
+++ TempMail/TempMail.py
@@ -61,7 +61,7 @@ class TempMail:
         if data["email"] == None:
             return # ["None"]
         else:
-            emails = None
+            emails = ""
             for email in data["email"]:
                 emails.append(Email(email["from"], email["to"], email["subject"], email["body"], email["html"], email["date"]))
             return emails
