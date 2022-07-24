8c6e51f14b148226fd00b949d91bde259ee721ba
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-24 16:15:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
         long_description = "\n" + fh.read()
 
 setup(name='tempmail-lol',
-        version='1.1.4',
+        version='1.1.5',
         description='A Python API for TempMail',
         author='Alex Torres',
         author_email='cloudbotsedc@gmail.com',
