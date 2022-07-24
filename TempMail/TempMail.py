6cff9723fdce061456c818f67828b8fb40fa3736
Github-noob <86904318+github-noob-arjun@users.noreply.github.com>
2022-07-24 14:12:17 +0530

    Update TempMail.py

--- TempMail/TempMail.py
+++ TempMail/TempMail.py
@@ -61,8 +61,20 @@ class TempMail:
         if data["email"] == None:
             return ["None"]
         else:
-            emails = []
+            #emails = []
+            subject
+            from
+            to
+            body
+            html
+            date
             for email in data["email"]:
-                emails.append(Email(email["from"], email["to"], email["subject"], email["body"], email["html"], email["date"]))
+                #emails.append(Email(email["from"], email["to"], email["subject"], email["body"], email["html"], email["date"]))
+                subject.append(Email(email["subject"]))
+                from.append(Email(email["from"]))
+                to.append(Email(email["to"]))
+                body.append(Email(email["body"]))
+                html.append(Email(email["html"]))
+                date.append(Email(email["date"]))
             return emails
         
